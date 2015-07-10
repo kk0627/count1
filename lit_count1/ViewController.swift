bc8e7e6e7353858a01916ab3ea93ca5cb97a7ffe
keita kutsuno <keita@keita-no-MacBook-Air.local>
2015-07-11 00:35:27 +0900

    first commit

--- lit_count1/ViewController.swift
+++ lit_count1/ViewController.swift
@@ -7,18 +7,49 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,AVAudioPlayerDelegate {
+    
+    @IBOutlet var myLabel: UILabel!
+    var number : Int = 0
+    var audioPlayer : AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //再生する音源のURLを生成
+        let soundFilePath : NSString = NSBundle.mainBundle().pathForResource("cat", ofType: "mp3")!
+        let fileURL : NSURL = NSURL(fileURLWithPath: soundFilePath as String)!
+        
+        audioPlayer = AVAudioPlayer(contentsOfURL: fileURL, error: nil)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func randomPlus() {
+        //1〜5までのランダムな数字
+        var randomNumber = Int(arc4random()%6)
+        number += randomNumber
+        
+        if number%7 == 0 {
+            self.view.backgroundColor = UIColor.blackColor()
+            myLabel.textColor = UIColor.whiteColor()
+            myLabel.text = String(number)
+            audioPlayer.numberOfLoops = 1
+            audioPlayer.play()
+        }else{
+            self.view.backgroundColor = UIColor.whiteColor()
+            myLabel.textColor = UIColor.blackColor()
+            myLabel.text = String(number)
+        }
+    }
+    
 
 
 }
